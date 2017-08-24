7e3d6a2b2a75e9d2c20e2268a651ce8b91d9e3d5
Iman Muliawan <imanmuliawan@gmail.com>
2017-08-24 15:22:40 +0700

    commit ke 1 UAS P3Java

---
+++ src/piiiuascuaca/PIIIUasCuaca.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package piiiuascuaca;
+import piiiuascuaca.view.FrmCuaca;
+/**
+ *
+ * @author IMAN-IT
+ */
+public class PIIIUasCuaca {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FrmCuaca frmCuaca = new FrmCuaca();
+        frmCuaca.setLocationRelativeTo(null);
+        frmCuaca.setVisible(true);
+    }
+    
+}
